b973a7f8fd0ddd81f1861497d38df339f017b7f1
nhuhuyen51 <108323933+nhuhuyen51@users.noreply.github.com>
2022-06-29 17:02:59 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,6 @@ terraform {
       version = "~> 4.16"
     }
   }
-  provider "aws" {
-    region  = "us-west-2"
-  }
 
   required_version = ">= 1.2.0"
   cloud {
@@ -18,3 +15,6 @@ terraform {
     }
   }
 }
+provider "aws" {
+    region  = "us-west-2"
+  }
